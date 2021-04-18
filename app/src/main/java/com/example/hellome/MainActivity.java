64be19a9b0d1d0489da6769f09759eb636499129
YOYZHANG <alexuzhang@gmail.com>
2021-04-18 16:49:37 +0800

    android-init

---
+++ app/src/main/java/com/example/hellome/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.hellome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView.setWebViewClient(new offLineWebClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.loadUrl("http://www.baidu.com");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
---
+++ app/src/main/java/com/example/hellome/offLineWebClient.java
@@ -0,0 +1,58 @@
+package com.example.hellome;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.FileUtils;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class offLineWebClient extends WebViewClient {
+    Context myContext;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view,
+                                                      WebResourceRequest request) {
+        final String url = request.getUrl().toString();
+        WebResourceResponse resourceResponse = getWebResourceResponse(url);
+
+        if (resourceResponse == null) {
+            return super.shouldInterceptRequest(view, request);
+        }
+
+        return resourceResponse;
+    }
+
+    private WebResourceResponse getWebResourceResponse(String url) {
+        try {
+            WebResourceResponse resourceResponse = getCssWebResourceResponseFromAsset();
+            return resourceResponse;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return WebResourceResponse with CSS markup from an asset (e.g. "assets/style.css").
+     */
+    private WebResourceResponse getCssWebResourceResponseFromAsset() {
+        try {
+            return getUtf8EncodedCssWebResourceResponse(myContext.getAssets().open("index.html"));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+
+    private WebResourceResponse getUtf8EncodedCssWebResourceResponse(InputStream data) {
+        return new WebResourceResponse("text/html", "UTF-8", data);
+    }
+}
